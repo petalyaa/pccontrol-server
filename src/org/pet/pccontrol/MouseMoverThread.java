90e31639e2d3a34df9b85d55974c05e22352abdf
Mohd Khairul Ikhwan Kamarudin <petalyaa@gmail.com>
2013-02-14 11:48:03 +0800

    Clean unuse code

--- src/org/pet/pccontrol/MouseMoverThread.java
+++ src/org/pet/pccontrol/MouseMoverThread.java
@@ -6,7 +6,6 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,41 +17,38 @@ import javax.swing.text.BadLocationException;
 import org.pet.pccontrol.MouseButtonClicker.ButtonAction;
 
 class MouseMoverThread extends Thread {
-	
+
 	private BufferedInputStream inputStream;
-	
-	private BufferedOutputStream outputStream;
-        
-        private JTextArea textArea;
-	
-	public MouseMoverThread(InputStream inputStream, OutputStream outputStream, JTextArea textArea){
+
+	private JTextArea textArea;
+
+	public MouseMoverThread(InputStream inputStream, OutputStream outputStream,
+			JTextArea textArea) {
 		this.inputStream = new BufferedInputStream(inputStream);
-		this.outputStream = new BufferedOutputStream(outputStream);
-                this.textArea = textArea;
+		this.textArea = textArea;
 	}
-	
-        private String getLogString(String newLog){
-            String existing = textArea.getText();
-            StringBuilder sb = new StringBuilder(existing);
-            sb.append("\n").append(newLog);
-            return sb.toString();
-        }
-        
-        private void scrollToBottom(){
-            try {
-               int endPosition = textArea.getDocument().getLength();
-               Rectangle bottom = textArea.modelToView(endPosition);
-               textArea.scrollRectToVisible(bottom);
-           }
-           catch (BadLocationException e) {
-               System.err.println("Could not scroll to " + e);
-           }
-        }
-        
-	public void run(){
-            textArea.setText(getLogString("Start listenning..."));
-            scrollToBottom();
-		while(true){
+
+	private String getLogString(String newLog) {
+		String existing = textArea.getText();
+		StringBuilder sb = new StringBuilder(existing);
+		sb.append("\n").append(newLog);
+		return sb.toString();
+	}
+
+	private void scrollToBottom() {
+		try {
+			int endPosition = textArea.getDocument().getLength();
+			Rectangle bottom = textArea.modelToView(endPosition);
+			textArea.scrollRectToVisible(bottom);
+		} catch (BadLocationException e) {
+			System.err.println("Could not scroll to " + e);
+		}
+	}
+
+	public void run() {
+		textArea.setText(getLogString("Start listenning..."));
+		scrollToBottom();
+		while (true) {
 			InputStreamReader reader = null;
 			BufferedReader bufferedReader = null;
 			String line;
@@ -60,18 +56,18 @@ class MouseMoverThread extends Thread {
 				reader = new InputStreamReader(inputStream);
 				bufferedReader = new BufferedReader(reader);
 				boolean isQuitNeeded = false;
-				while((line = bufferedReader.readLine()) != null){
+				while ((line = bufferedReader.readLine()) != null) {
 					Robot robot = new Robot();
 					line = line.toLowerCase().trim();
-					if(line.startsWith("button=")){
+					if (line.startsWith("button=")) {
 						String[] buttonStrArr = line.split("\\=");
 						int actionInt = Integer.parseInt(buttonStrArr[1]);
 						MouseButtonClicker clicker = new MouseButtonClicker();
-						switch(actionInt){
-						case 1 :
+						switch (actionInt) {
+						case 1:
 							clicker.click(ButtonAction.LEFT_CLICK);
 							break;
-						case 2 :
+						case 2:
 							clicker.click(ButtonAction.RIGHT_CLICK);
 							break;
 						}
@@ -79,19 +75,20 @@ class MouseMoverThread extends Thread {
 						String[] coordinateArr = line.split("\\,");
 						int x = Integer.parseInt(coordinateArr[0]);
 						int y = Integer.parseInt(coordinateArr[1]);
-						
-						Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
+
+						Point mouseLocation = MouseInfo.getPointerInfo()
+								.getLocation();
 						int currentX = (int) mouseLocation.getX();
 						int currentY = (int) mouseLocation.getY();
-						
+
 						robot.mouseMove(currentX + x, currentY + y);
-						
-						if(line.equals("quit")){
+
+						if (line.equals("quit")) {
 							isQuitNeeded = true;
 						}
 					}
 				}
-				if(isQuitNeeded)
+				if (isQuitNeeded)
 					break;
 			} catch (IOException e) {
 				break;
@@ -99,21 +96,21 @@ class MouseMoverThread extends Thread {
 				break;
 			} finally {
 				try {
-					if(bufferedReader != null)
+					if (bufferedReader != null)
 						bufferedReader.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 				try {
-					if(reader != null)
+					if (reader != null)
 						reader.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		 textArea.setText(getLogString("End listenning..."));
-                 scrollToBottom();
+		textArea.setText(getLogString("End listenning..."));
+		scrollToBottom();
 	}
-	
+
 }
